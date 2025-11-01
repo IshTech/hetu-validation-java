0d14f4edd72513b87d0a747126e48fccc32551ad
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-11-01 10:40:56 +0200

    pom - javax-validation replaced with jakarta-validation

--- src/main/java/fi/ishtech/validation/constraints/HeTu.java
+++ src/main/java/fi/ishtech/validation/constraints/HeTu.java
@@ -16,8 +16,8 @@ import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import fi.ishtech.validation.constraints.HeTu.List;
 import fi.ishtech.validation.constraints.validator.HeTuValidator;
